938648c810315a8949c7c4798ace33d2526ffc4d
Millefeuille42 <mlabouri@student.42.fr>
2020-08-05 19:14:59 +0200

    Fixed - problems in name

--- discordapi.go
+++ discordapi.go
@@ -114,7 +114,7 @@ func messageHandler(session *discordgo.Session, message *discordgo.MessageCreate
 	}
 
 	if strings.HasPrefix(message.Content, "!register") {
-		args := strings.Split(message.Content, "-")
+		args := strings.Split(message.Content, "|")
 		if len(args) == 4 {
 			_ = registerUser(session, message, args)
 		}
