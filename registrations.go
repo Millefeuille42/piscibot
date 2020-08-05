915423e03c7cb62971f52978deeb7b10b38cf7d6
Millefeuille42 <mlabouri@student.42.fr>
2020-08-05 17:49:48 +0200

    Created registration Function

--- registrations.go
+++ registrations.go
@@ -4,13 +4,79 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"github.com/bwmarrin/discordgo"
 	"io/ioutil"
 	"os"
 )
 
 type userRegistrationFile struct {
-	User  string
-	Pisci string
+	Login  string
+	Pisci  string
+	Mail   string
+	UserID string
+}
+
+func checkRegistrationError(session *discordgo.Session, message *discordgo.MessageCreate, args []string) error {
+	_, loginExist := os.Stat(fmt.Sprintf("./data/registrations/%s.json", args[1]))
+	_, pisciExist := os.Stat(fmt.Sprintf("./data/targets/%s.json", args[2]))
+	if loginExist == nil {
+		_, _ = session.ChannelMessageSend(message.ChannelID,
+			fmt.Sprintf("<@%s> %s is already registered!", message.Author.ID, message.Author.ID))
+		return os.ErrExist
+	}
+	if pisciExist == nil {
+		_, _ = session.ChannelMessageSend(message.ChannelID,
+			fmt.Sprintf("<@%s> %s is already taken!", message.Author.ID, args[2]))
+		return os.ErrExist
+	}
+	return nil
+}
+
+func createRegistrationFile(message *discordgo.MessageCreate, args []string) error {
+	userRegistration := userRegistrationFile{
+		Login:  args[1],
+		Pisci:  args[2],
+		Mail:   args[3],
+		UserID: message.Author.ID,
+	}
+
+	registrationJson, err := json.MarshalIndent(userRegistration, "", "\t")
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(fmt.Sprintf("./data/registrations/%s.json", message.Author.ID))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	err = ioutil.WriteFile(fmt.Sprintf("./data/registrations/%s.json", message.Author.ID),
+		registrationJson, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func registerUser(session *discordgo.Session, message *discordgo.MessageCreate, args []string) error {
+	if err := checkRegistrationError(session, message, args); err != nil {
+		return err
+	}
+	if err := createRegistrationFile(message, args); err != nil {
+		_, _ = session.ChannelMessageSend(message.ChannelID, err.Error())
+		return err
+	}
+	f, err := os.OpenFile("./data/registrations/userlist.txt",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		_, _ = session.ChannelMessageSend(message.ChannelID, err.Error())
+		return err
+	}
+	defer f.Close()
+	if _, err := f.WriteString(fmt.Sprintf("%s\n", message.Author.ID)); err != nil {
+		_, _ = session.ChannelMessageSend(message.ChannelID, err.Error())
+		return err
+	}
+	return nil
 }
 
 func getPisciList() ([]string, error) {
